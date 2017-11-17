df86732d74f51714fe9ce4b39ffb82197f3efa78
sg <sglee487@gmail.com>
2017-11-18 00:02:14 +0900

    update several function

--- src/Calculate/Calculate.java
+++ src/Calculate/Calculate.java
@@ -38,17 +38,17 @@ public class Calculate {
 
 	public static int sub(int x, int y) {
 
-		return 0;
+		return x-y;
 	}
 
 	public static int mul(int x, int y) {
 
-		return 0;
+		return x*y;
 	}
 
 	public static double div(int x, int y) {
 		// 실수로 반환이 되도록 구현
 
-		return 0;
+		return x/y;
 	}
 }
